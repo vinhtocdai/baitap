292ea6b800e85585900d8a1220499673f87299cf
Tran Thanh Vinh <56049480+vinhtocdai@users.noreply.github.com>
2020-09-15 22:44:43 +0700

    hcn

---
+++ Header.h
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+struct HinhChuNhat
+{
+	float ChieuDai;
+	float ChieuRong;
+};
+void nhap(HinhChuNhat &hcn);
+void xuat(HinhChuNhat hcn);
+float tinhChuVi(HinhChuNhat hcn);
+float tinhDienTich(HinhChuNhat hcn);
